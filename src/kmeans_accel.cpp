e5401b0b8f6b81097cd5c283203fda7417eae14a
Xingtang Sun <61231691+xing18223@users.noreply.github.com>
2022-04-25 09:48:05 +0100

    Add files via upload

--- src/kmeans_accel.cpp
+++ src/kmeans_accel.cpp
@@ -1,3 +1,6 @@
+// Author: Xingtang Sun - ni18223@bristol.ac.uk
+// Date: 25th April 2022
+
 #include "sds_lib.h"
 #include "knl_kmeans.h"
 #include <sys/ioctl.h>
--- src/knl_kmeans.h
+++ src/knl_kmeans.h
@@ -1,2 +1,5 @@
+// Author: Xingtang Sun - ni18223@bristol.ac.uk
+// Date: 25th April 2022
+
 void knl_kmeans(float *points, float *clusters, float *dist_out, int k, int dim, int begin, int end, int file_desc, int ioctl_flag);
 
